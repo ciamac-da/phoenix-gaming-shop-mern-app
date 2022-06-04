ded3d322f051715465b05862ba3f94e5443689e1
ciamac-da <ciamac.davoudi@gmail.com>
2022-06-04 23:32:31 +0200

    Verified by Marti4n v1.1.0 starting setup, pages,routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div className="App">
-    Hello World :D
-    </div>
-  );
+import React from "react";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Users from "./user/pages/Users";
+
+const App = () => {
+  return <Router>
+    <Route path="/" exact>
+    <Users />
+    </Route>
+  </Router>
 }
 
 export default App;
